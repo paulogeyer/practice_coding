d253ca3d67b28222cf0e12c90afc3ba174b92b36
Paulo Geyer <paulogeyer@gmail.com>
2021-08-19 15:16:34 -0300

    forgot to commit some changes

--- sicp/ch02/2.2.scm
+++ sicp/ch02/2.2.scm
@@ -477,3 +477,81 @@
 
 (define (reverse sequence)
   (fold-left (lambda (x y) (cons y x)) nil sequence))
+
+;; nested mappings
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+	       (flatmap
+		(lambda (i)
+		  (map (lambda (j) (list i j))
+		       (enumerate-interval 1 (- i 1))))
+		(enumerate-interval 1 n)))))
+
+(define (permutations s)
+  (if (null? s)
+      (list nil)
+      (flatmap (lambda (x)
+		 (map (lambda (p) (cons x p))
+		      (permutations (remove x s))))
+	       s)))
+
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item)))
+	  sequence))
+
+;; ex 2.40
+(define (unique-pairs n)
+  (flatmap
+   (lambda (i)
+     (map (lambda (j) (list i j))
+	  (enumerate-interval 1 (- i 1))))
+   (enumerate-interval 1 n)))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+	       (unique-pairs n))))
+
+;; ex 2.41
+(define (sum-triplets n k)
+  (define (unique-triplets n)
+    (flatmap (lambda (i)
+		     (flatmap
+		      (lambda (j)
+			(map
+			 (lambda (k)
+			   (list i j k))
+			 (enumerate-interval (+ j 1) n)))
+		      (enumerate-interval (+ i 1) (- n 1))))
+		   (enumerate-interval 1 n)))
+  (filter (lambda (x)
+	    (= k (fold-right + 0 x)))
+	  (unique-triplets n)))
+
+;; ex 2.42
+(define empty-board '())
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+	(list empty-board)
+	(filter
+	 (lambda (positions) (safe? k position))
+	 (flatmap
+	  (lambda (rest-of-queens)
+	    (map (lambda (new-row)
+		   (adjoin-position new-row k rest-of-queens))
+		 (enumerate-interval 1 board-size)))
+	  (queen-cols (- k 1))))))
+  (queen-cols board-size))
